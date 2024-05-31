4cc7ed0cb1cae38562f8f26ceebb8c8a0699d031
Brendan Xiong <32696639+brendanxiong@users.noreply.github.com>
2024-05-30 18:26:06 -0700

    work on dev environment set up

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
-const isProduction = process.env.NODE_ENV == 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
 const stylesHandler = MiniCssExtractPlugin.loader;
 
@@ -38,6 +38,7 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
       },
+      /* ** WILL NEED A TS LOADER AND BABEL FOR JS LOADER ** */
 
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
