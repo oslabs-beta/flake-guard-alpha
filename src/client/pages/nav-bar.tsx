cae8d617fb097f9acd5afee304cd38ec0da72f16
Ashley Hannigan <ashleyhannigan88@gmail.com>
2024-06-01 17:12:02 -0400

    routes working

--- src/client/pages/nav-bar.tsx
+++ src/client/pages/nav-bar.tsx
@@ -3,6 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 //bootstrap template
 //type React Functional Componenet
@@ -10,16 +11,24 @@ const NavBarHeading: React.FC = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary navbar-custom">
       <Container>
-        <Navbar.Brand href="#home">Flake-Guard</Navbar.Brand>
+        <Navbar.Brand>
+          <Link to="/home">Flake-Guard</Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             {/* <Nav.Link href="#home">Home</Nav.Link> */}
             <NavDropdown title="Dashboard" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#analytics">Analytics</NavDropdown.Item>
-              <NavDropdown.Item href="#profile">Profile</NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link to="/analytics">Analytics</Link>
+              </NavDropdown.Item>
+              {/* <NavDropdown.Item>
+                <Link to="/profile">Profile</Link>
+              </NavDropdown.Item> */}
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#login">Login / Logout</NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link to="/login">Login / Logout</Link>
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
