a9c8706ebe00069cd52c5a1de5a294dc0c40836c
Ashley Hannigan <ashleyhannigan88@gmail.com>
2024-06-06 23:06:48 -0400

    code image added

--- src/client/pages/landing-page.tsx
+++ src/client/pages/landing-page.tsx
@@ -4,20 +4,25 @@ import NavBarHeading from '../components/nav-bar';
 import GetStarted from '../components/LandingPage/get-started';
 import lightBulb from '../assets/lightbulb.png';
 import Carousel from '../components/landing-carousel';
+import code_image from '../assets/code_2.png';
+
 const Landing: React.FC = () => {
   return (
     <>
       <header>
         <NavBarHeading />
       </header>
- 
       <div className="intro-container">
         <h1 id="intro-title">flake-guard</h1>
-        <br/>
+        <br />
         <GetStarted />
       </div>
       <br />
       <Carousel />
+      <img src={code_image}></img>
+      <section>
+        <img src={lightBulb}></img>
+      </section>
       <p
         style={{
           display: 'flex',
@@ -28,7 +33,6 @@ const Landing: React.FC = () => {
         Insert documentation page info here as a scrolling option but for the
         landing page but also a direct link with the endpoint or header
       </p>
-        <section><img src={lightBulb}></img></section>
       <footer></footer>{' '}
     </>
   );
