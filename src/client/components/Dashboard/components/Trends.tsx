39ef81ed4815e30ccd7080108b9b87739d518b71
Ashley Hannigan <ashleyhannigan88@gmail.com>
2024-06-15 12:44:40 -0400

    dash

--- src/client/components/Dashboard/components/Trends.tsx
+++ src/client/components/Dashboard/components/Trends.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 const Trends: React.FC = () => {
   return (
     <div>
@@ -8,7 +7,6 @@ const Trends: React.FC = () => {
       <p>Flakiness</p>
       <p>New Failures</p>
       <p>Always Failing</p>
-      
     </div>
   );
 };
