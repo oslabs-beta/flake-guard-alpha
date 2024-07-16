98d4d159ba19a2e6dad3be218258100495a82826
Paloma Reynolds <palomap.paula@hotmail.com>
2024-07-15 21:52:20 -0400

    feat: add style to the docs page

--- flake-guard-app/src/client/components/Login/login-out.tsx
+++ flake-guard-app/src/client/components/Login/login-out.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import NavBarHeading from '../nav-bar';
-import '../../../styles/styles.css';
+// import React from 'react';
+// import NavBarHeading from '../nav-bar';
+// import '../../../styles/styles.css';
 
-const LoginOrOut: React.FC = () => {
-  return (
-    <>
-      <div className="App-header">
-        <NavBarHeading />
-      </div>
-      <div className="login-container">
-        <div className="login-text"></div>
-      </div>
-    </>
-  );
-};
+// const LoginOrOut: React.FC = () => {
+//   return (
+//     <>
+//       <div className="App-header">
+//         <NavBarHeading />
+//       </div>
+//       <div className="login-container">
+//         <div className="login-text"></div>
+//       </div>
+//     </>
+//   );
+// };
 
-export default LoginOrOut;
+// export default LoginOrOut;
