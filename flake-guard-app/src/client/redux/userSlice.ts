c8f19b6a77ab1e2a51c76fd507bdcf147324133e
Will Suto <will.suto@gmail.com>
2024-07-16 19:07:01 -0700

    wip

--- flake-guard-app/src/client/redux/userSlice.ts
+++ flake-guard-app/src/client/redux/userSlice.ts
@@ -31,6 +31,12 @@ export const userSlice = createSlice({
     //     console.log('Error Fetching User:', error);
     //   }
     // },
+    userSignedIn: (state: User): void => {
+      
+    },
+    userLoggedOut: (state: User): void => {
+
+    }
   },
 });
 
