2a272bcd8e454616f87de29b3ed3bd2949f5334b
Ashley Hannigan <ashleyhannigan88@gmail.com>
2024-07-25 16:45:16 -0400

    faq testing

--- flake-guard-app/src/client/components/Docs/FAQ.tsx
+++ flake-guard-app/src/client/components/Docs/FAQ.tsx
@@ -4,8 +4,8 @@ import '../../../styles/docs.css';
 const Faq = (): JSX.Element => {
   return (
     <div>
-      <div className="accordion accordion-flush" id="accordionFlushExample">
-        <div className="accordion-item faq-container" data-testid = 'faq container'>
+      <div className="accordion accordion-flush" id="accordionFlushExample" data-testid="faq-section">
+        <div className="accordion-item faq-container" >
           <h2 className="accordion-header" id="flush-headingOne">
             <button
               className="accordion-button collapsed custom-bg"
@@ -14,9 +14,8 @@ const Faq = (): JSX.Element => {
               data-bs-target="#flush-collapseOne"
               aria-expanded="false"
               aria-controls="flush-collapseOne"
-              aria-label="faq-button"
             >
-              What is a flaky test?
+              What is a flakey test?
             </button>
           </h2>
           <div
@@ -25,8 +24,8 @@ const Faq = (): JSX.Element => {
             aria-labelledby="flush-headingOne"
             data-bs-parent="#accordionFlushExample"
           >
-            <div className="accordion-body" aria-label="faq-text">
-              A flaky test is a test that sometimes passes and sometimes fails
+            <div className="accordion-body" >
+              A flakey test is a test that sometimes passes and sometimes fails
               for the same code, often due to nondeterministic factors like
               timing issues, network variability, or reliance on external
               systems..
@@ -42,9 +41,8 @@ const Faq = (): JSX.Element => {
               data-bs-target="#flush-collapseTwo"
               aria-expanded="false"
               aria-controls="flush-collapseTwo"
-              aria-label="faq-button"
             >
-              What causes flaky tests?
+              What causes flakey tests?
             </button>
           </h2>
           <div
@@ -53,9 +51,8 @@ const Faq = (): JSX.Element => {
             aria-labelledby="flush-headingTwo"
             data-bs-parent="#accordionFlushExample"
           >
-            <div className="accordion-body"
-            aria-label="faq-text">
-              Flaky tests can be caused by race conditions, timing issues,
+            <div className="accordion-body">
+              Flakey tests can be caused by race conditions, timing issues,
               network instability, environmental dependencies, or inconsistent
               external service responses.
             </div>
@@ -70,7 +67,6 @@ const Faq = (): JSX.Element => {
               data-bs-target="#flush-collapseThree"
               aria-expanded="false"
               aria-controls="flush-collapseThree"
-              aria-label="faq-button"
             >
               When should I use FlakeGuard?
             </button>
@@ -81,9 +77,7 @@ const Faq = (): JSX.Element => {
             aria-labelledby="flush-headingThree"
             data-bs-parent="#accordionFlushExample"
           >
-            <div className="accordion-body"
-            aria-label="faq-text">
-              You should use a FlakeGuard when you notice intermittent test
+            <div className="accordion-body">              You should use a FlakeGuard when you notice intermittent test
               failures, to identify and address unstable tests, ensuring more
               reliable and consistent test results.
             </div>
@@ -98,9 +92,8 @@ const Faq = (): JSX.Element => {
               data-bs-target="#flush-collapseFour"
               aria-expanded="false"
               aria-controls="flush-collapseFour"
-              aria-label="faq-button"
             >
-              What's the impact of flaky tests?
+              What's the impact of flakey tests?
             </button>
           </h2>
           <div
@@ -109,9 +102,7 @@ const Faq = (): JSX.Element => {
             aria-labelledby="flush-headingFour"
             data-bs-parent="#accordionFlushExample"
           >
-            <div className="accordion-body"
-            aria-label="faq-text">
-              <p>
+            <div className="accordion-body">              <p>
                 <b>Reduced Confidence:</b>
                 They undermine trust in the test suite, as developers can't rely
                 on test results to reflect the true state of the code.
@@ -148,9 +139,8 @@ const Faq = (): JSX.Element => {
               data-bs-target="#flush-collapseFive"
               aria-expanded="false"
               aria-controls="flush-collapseFive"
-              aria-label="faq-button"
             >
-              What are the best practices to prevent flaky tests?
+              What are the best practices to prevent flakey tests?
             </button>
           </h2>
           <div
@@ -159,9 +149,7 @@ const Faq = (): JSX.Element => {
             aria-labelledby="flush-headingFive"
             data-bs-parent="#accordionFlushExample"
           >
-            <div className="accordion-body"
-            aria-label="faq-text">
-              To prevent flaky tests, ensure tests are isolated to avoid shared
+            <div className="accordion-body">              To prevent flakey tests, ensure tests are isolated to avoid shared
               state dependencies. Control timing with fixed timeouts and avoid
               relying on fluctuating conditions. Mock external dependencies for
               consistent interactions and reduce variability. Use stable,
@@ -181,7 +169,6 @@ const Faq = (): JSX.Element => {
               data-bs-target="#flush-collapseSix"
               aria-expanded="false"
               aria-controls="flush-collapseSix"
-              aria-label="faq-button"
             >
               How well does FlakeGuard “scale” in terms of performance?
             </button>
@@ -193,7 +180,7 @@ const Faq = (): JSX.Element => {
             data-bs-parent="#accordionFlushExample"
           >
             <div className="accordion-body"
-            aria-label="faq-text">....</div>
+            >....</div>
           </div>
         </div>
       </div>
--- flake-guard-app/src/client/components/Docs/faq.test.tsx
+++ flake-guard-app/src/client/components/Docs/faq.test.tsx
@@ -3,13 +3,27 @@
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom'; // Import the matchers
 import Faq from './FAQ';
 
-describe('FAQ', () => {
-    it('renders FAQ component', () => {
-        render (<Faq />)
-    })
-    it('should include all faq questions and answers', () => {
-         userEvent.click(screen.getByRole('button', {name: 'faq-button'}))
-    })
+describe('FAQ',  () => {
+    test('renders FAQ questions', () => {
+        render(<Faq />);
+        
+  // Assert that the FAQ section is open
+  const firstFaq = screen.getByText('What is a flakey test?');
+  expect(firstFaq).toBeTruthy();
+  
+  const firstAnswer = screen.getByText('A flakey test is a test that sometimes passes and sometimes fails for the same code, often due to nondeterministic factors like timing issues, network variability, or reliance on external systems..')
+  expect(firstAnswer).toBeTruthy();
+
+  const secondFaq = screen.getByText('What causes flakey tests?');
+  expect(secondFaq).toBeTruthy();
+
+  const thirdFaq = screen.getByText('When should I use FlakeGuard?');
+  expect(thirdFaq).toBeTruthy();
+
+  const fourthFaq = screen.getByText(`What's the impact of flakey tests?`);
+  expect(fourthFaq).toBeTruthy();
+    });
 });
