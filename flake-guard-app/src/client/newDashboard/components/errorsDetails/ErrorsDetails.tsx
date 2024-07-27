d63f44eaa3a2b13f04670019454d4ccfeb01c9ce
Paloma Reynolds <palomap.paula@hotmail.com>
2024-07-27 12:56:00 -0400

    adding duration chart

--- flake-guard-app/src/client/newDashboard/components/errorsDetails/ErrorsDetails.tsx
+++ flake-guard-app/src/client/newDashboard/components/errorsDetails/ErrorsDetails.tsx
@@ -43,37 +43,3 @@ const ErrorsDetails: React.FC = ({results}) => {
 }
 
 export default ErrorsDetails;
-
-{/* <div className='errors-container'>
-<p>Errors</p>
-{errorsDetails &&
-<ul>
- {errorsDetails.map(el => (
-     <li key={el.id}>
-         {el.id}
-         {el.fullName}
-     </li> // Changed <ol> to <li> and added key prop
- ))}
-</ul>
-}
-</div>
-) */}
-
-
-// <Accordion>
-// {errorsDetails && errorsDetails.map(error => (
-//     <Accordion.Item eventKey={error.id.toString()} key={error.id}>
-//         <Accordion.Header>{error.id} + {error.fullName}</Accordion.Header>
-//             {metrics && metrics.map((test, index) => (
-//             <Accordion.Body>
-
-//                 <div key={index}>
-//                     <p>{test.fullName}</p>
-//                     <p>Passed{test.passed}</p>
-//                 </div>
-//                 </Accordion.Body>
-
-//             ))}
-//     </Accordion.Item>
-// ))}
-// </Accordion>
--- flake-guard-app/src/client/newDashboard/dashboard/NewUserDashboard.tsx
+++ flake-guard-app/src/client/newDashboard/dashboard/NewUserDashboard.tsx
@@ -16,6 +16,7 @@ import LineChart from '../components/line/LineChart';
 import {flakyDataParser} from '../../utilities/flakyDataParser';
 import {OverlayTrigger, Tooltip} from 'react-bootstrap';
 import LoginButton from '../../components/Login/LoginButton';
+import Duration from '../components/duration/Duration';
 
 const NewUserDashboard: React.FC = () => {
   const {userId} = useParams();
@@ -145,12 +146,15 @@ const NewUserDashboard: React.FC = () => {
         </div>
         <div className="bottom-content">
           {/* BOTTOM CONTENT */}
-          <div
-            className="linechart-graph graph-style"
-            style={{height: '250px', width: '550px'}}
-          >
-            <LineChart results={results} />
+          <div>
+              <Duration/>
+            <div
+              className="linechart-graph graph-style"
+              style={{height: '250px', width: '550px'}}
+            >
+              <LineChart results={results} />
 
+            </div>
           </div>
           <div className='graph-style errors-details'  style={{height: '450px', width: '50%'}}
 > 
