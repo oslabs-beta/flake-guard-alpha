d5b4d35c0c73715ae4c2a69bc920926016fb8ddf
Ashley Hannigan <ashleyhannigan88@gmail.com>
2024-07-27 15:30:08 -0400

    landing page basic styling and info, missing tommys info fully

--- flake-guard-app/src/client/components/LandingPage/MeetTheTeam.tsx
+++ flake-guard-app/src/client/components/LandingPage/MeetTheTeam.tsx
@@ -6,54 +6,68 @@ import ashley from '../../assets/ashley.png';
 import paloma from '../../assets/paloma.png';
 import will from '../../assets/will.png';
 import brendan from '../../assets/brendan.png';
+import linkedInLogo from '../../assets/linkedinlogo.png'; 
+//NEEDING TOMMY INFO -> LINKEDIN AND IMAGE
+//STRETCH FEATURES -> WHEN CLICKING OUR TEAM -> GET DIRECTED TO THIS SPOT ON THE PAGE]
+
 
 const TeamMember = () => {
     const name= ["Ashley", "Paloma", "Tommy", "Brendan", "Will"];
     const role="Full Stack Developer"
     return (
         <>
-      <div className="team-member">
+      <div className="team-member" id="avatars">
         <img src={ashley} alt={name[0]} className="team-member-image" />
         <h2>{name[0]}</h2>
         <p>{role}</p>
         <div className="social-links">
-            <p>https://www.linkedin.com/in/ashley-hannigan-88-/</p>
+            <a href="https://www.linkedin.com/in/ashley-hannigan-88-/" target="_blank" rel="noopener noreferrer">
+                <img src={linkedInLogo} alt="LinkedIn" id="linkedinlogo"/>
+            </a> 
           {/* Add social links here */}
         </div>
       </div>
-        <div className="team-member">
+        <div className="team-member" id="avatars">
         <img src={paloma} alt={name[1]} className="team-member-image" />
         <h2>{name[1]}</h2>
         <p>{role}</p>
         <div className="social-links">
-            <p>https://www.linkedin.com/in/palomareynolds/</p>
+            <a href="https://www.linkedin.com/in/palomareynolds/" target="_blank" rel="noopener noreferrer">
+                <img src={linkedInLogo} alt="LinkedIn" id="linkedinlogo"/>
+            </a> 
           {/* Add social links here */}
         </div>
       </div>
-        <div className="team-member">
+        <div className="team-member" id="avatars">
         <img src={ashley} alt={name[2]} className="team-member-image" />
         <h2>{name[2]}</h2>
         <p>{role}</p>
         <div className="social-links">
-            <p>https://www.linkedin.com/in/ashley-hannigan-88-/</p>
+            <a href="https://www.linkedin.com/in/palomareynolds/" target="_blank" rel="noopener noreferrer">
+                <img src={linkedInLogo} alt="LinkedIn" id="linkedinlogo"/>
+            </a> 
           {/* Add social links here */}
         </div>
       </div>
-      <div className="team-member">
+      <div className="team-member" id="avatars">
         <img src={brendan} alt={name[3]} className="team-member-image" />
         <h2>{name[3]}</h2>
         <p>{role}</p>
         <div className="social-links">
-            <p>https://www.linkedin.com/in/ashley-hannigan-88-/</p>
+            <a href="https://www.linkedin.com/in/brendanxiong/" target="_blank" rel="noopener noreferrer">
+                <img src={linkedInLogo} alt="LinkedIn" id="linkedinlogo"/>
+            </a> 
           {/* Add social links here */}
         </div>
       </div>
-      <div className="team-member">
+      <div className="team-member" id="avatars">
         <img src={will} alt={name[4]} className="team-member-image" />
         <h2>{name[4]}</h2>
         <p>{role}</p>
         <div className="social-links">
-            <p>https://www.linkedin.com/in/willsuto/</p>
+            <a href="https://www.linkedin.com/in/willsuto/" target="_blank" rel="noopener noreferrer">
+                <img src={linkedInLogo} alt="LinkedIn" id="linkedinlogo"/>
+            </a> 
           {/* Add social links here */}
         </div>
       </div>
