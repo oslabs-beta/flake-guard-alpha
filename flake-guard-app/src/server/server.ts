e7e5dccf7984207bd17a78100344b102222d72bb
Brendan Xiong <32696639+brendanxiong@users.noreply.github.com>
2024-08-14 10:37:35 -0700

    have server serve webpack files

--- flake-guard-app/src/server/server.ts
+++ flake-guard-app/src/server/server.ts
@@ -7,6 +7,7 @@ import resultsRouter from './routes/resultsRouter';
 import userDashRouter from './routes/userDashRouter';
 import tempDashRouter from './routes/tempDashRouter';
 import {errorHandler} from './errors/errorHandler';
+import {Request, Response} from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,16 +15,16 @@ const PORT = process.env.PORT || 3000;
 // Default middleware
 app.use(bodyParser.json({limit: '10mb'}));
 app.use(cors());
-app.use(express.static(path.resolve(__dirname, '../client')));
+app.use(express.static(path.resolve(__dirname, '../../dist')));
 
 // Endpoints
 app.use('/results', resultsRouter);
 app.use('/tempDash', tempDashRouter);
 app.use('/userDash', userDashRouter);
 
-// 404 error handler
-app.use((req, res) => {
-  res.status(404).send('Resource not found');
+// Catch all
+app.use('*', (req: Request, res: Response) => {
+  res.sendFile(path.resolve(__dirname, '../../dist/index.html'));
 });
 
 // Global error handler
